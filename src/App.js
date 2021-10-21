974c34837624f2fd241360ef8a85ca63a3c62615
Varun Badave <varunbadave99@gmail.com>
2021-10-21 21:04:00 +0530

    7s

--- src/App.js
+++ src/App.js
@@ -1,24 +1,101 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="card text-white bg-dark mb-3" >
+        <div className="card-header">7th Standard</div>
+        <div className="card-body">
+          <h3 className="card-title">सौ. रुपाली विनायक बडवे.(B.com,D.Ed)</h3>
+          <p className="card-text"> वर्ग ७वी (सहा. शिक्षिका) <br/>विषय : विज्ञान </p>
+          
+        </div>
+      </div>
+
+
+      <div className="varun">
+        <div className="polaroid">
+
+        <div className="container">
+                  <h2>7. गती, बल व कार्य</h2>
+                </div>
+                <div className="row">
+                    <div className="col-sm-6">
+                      <div className="card">
+                        <div className="card-body">
+                          <h5 className="card-title"> *प्रस्तावना*</h5>
+                          
+                          <a href="https://www.youtube.com/embed/RMGZDPXgFQ4" className="btn btn-primary">व्हीडिओ लिंक क्र.१</a>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-sm-6">
+                      <div className="card">
+                        <div className="card-body">
+                          <h5 className="card-title">*चाल व वेग*</h5>
+                          
+                          <a href="https://www.youtube.com/embed/qnvCfDqA9Jw" className="btn btn-primary">व्हीडिओ लिंक क्र.२</a>
+                        </div>
+                      </div>
+                    </div>
+                    
+                  </div>
+                  <br/>
+                  <div className="row">
+                    <div className="col-sm-6">
+                      <div className="card">
+                        <div className="card-body">
+                          <h5 className="card-title">*सरासरी वेग व तात्कालिक वेग*</h5>
+                          
+                          <a href="https://www.youtube.com/embed/iH7lvpRiEgQ" className="btn btn-primary">व्हीडिओ लिंक क्र.३</a>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-sm-6">
+                      <div className="card">
+                        <div className="card-body">
+                          <h5 className="card-title">*त्वरण* </h5>
+                          
+                          <a href="https://www.youtube.com/embed/uwY1ONKwXx4" className="btn btn-primary">व्हीडिओ लिंक क्र.४</a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                 
+                  <br/>
+                  <div className="row">
+                    <div className="col-sm-6">
+                      <div className="card">
+                        <div className="card-body">
+                          <h5 className="card-title">*न्यूटनचा गतिविषयक पहिला नियम*</h5>
+                        
+                          <a href="https://www.youtube.com/embed/dXnjyRXtHx0" className="btn btn-primary">व्हीडिओ लिंक क्र.५</a>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-sm-6">
+                      <div className="card">
+                        <div className="card-body">
+                          <h3 className="card-title">***स्वाध्याय***</h3>
+                        
+                          <a href="https://www.youtube.com/embed/vdgHHTSzNuc" className="btn btn-primary">व्हीडिओ लिंक क्र.६</a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                 
+                   
+                     <div>
+                    </div>
+                    
+                    
+
+                    </div>
     </div>
+    
+            
+    </>
   );
 }
 
